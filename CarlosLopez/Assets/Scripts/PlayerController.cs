ca18ed31ecca00b6a61473c7d59baa47d3bf3922
TristanIbanez16 <tristanjibanez@gmail.com>
2019-12-06 19:45:28 +1100

    Added object to samsple scene, started player controller

--- CarlosLopez/Assets/Scripts/PlayerController.cs
+++ CarlosLopez/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetAxis)
     }
 }
