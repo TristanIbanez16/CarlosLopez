2aa39068d1ddd853c9bad959e50a90ac579a7f28
TristanIbanez16 <tristanjibanez@gmail.com>
2019-12-06 20:32:09 +1100

    Working player controller, added black material

--- CarlosLopez/Assets/Scripts/PlayerController.cs
+++ CarlosLopez/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    CharacterController characterController;
+
+    [Header("Public Variables")]
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    [Header("Private Variables")]
+    private Vector3 moveDirection = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis)
+        if(characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+        }
+
+
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
     }
 }
